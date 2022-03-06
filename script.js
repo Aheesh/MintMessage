ca23bf7baf8cc4c99d4ad988478508546ffe1cec
aheeshnagraj <aheesh@gmail.com>
2022-03-06 14:43:40 -0800

    proj framework

---
+++ script.js
@@ -0,0 +1,12 @@
+async function loginWithEth() {
+  if (window.ethereum) {
+    window.web3 = new Web3(ethereum);
+    try {
+      await ethereum.enable();
+      var accounts = await web3.eth.getAccounts();
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+    alert('No Eth Browser Extension detected');
+  }
